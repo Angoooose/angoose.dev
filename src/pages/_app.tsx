72858935d082fd8eafe8211a18fb7eba76c1f64d
Angoose <contact@angoose.dev>
2023-06-03 10:02:54 -0500

    Add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Global from '@styles/global';
 import { theme } from '@styles/theme';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 
@@ -15,6 +16,14 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={theme}>
 			<Global/>
+			<Head>
+				<title>Angoose</title>
+				<meta name={'description'} content={'Full-Stack Software Engineer from the U.S.'} />
+				<meta name={'theme-color'} content={theme.colors.primaryGreen} />
+				<meta property={'og:site_name'} content={'Full-Stack Software Engineer'} />
+				<link rel="icon" href="/favicon.png" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
 			<div className={inter.className}>
 				{isMounted && <Component {...pageProps} />}
 			</div>
